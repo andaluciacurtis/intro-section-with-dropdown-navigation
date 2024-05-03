6853d788c630c273356a60676b2f4393aca04de8
Andalucia Curtis <143487070+andaluciacurtis@users.noreply.github.com>
2024-05-03 21:31:46 +0000

    fixed dropdown bugs

--- script.js
+++ script.js
@@ -2,27 +2,25 @@ const features = document.querySelector('.features-div');
 const company = document.querySelector('.company-div');
 const featuresDropdown = document.querySelector('.features-dropdown');
 const companyDropdown = document.querySelector('.company-dropdown');
-const featuresPrev = document.querySelector('.features-prev');
-const companyPrev = document.querySelector('.company-prev');
 const featuresArrow = document.querySelector('.features-arrow');
 const companyArrow = document.querySelector('.company-arrow');
 
-featuresPrev.addEventListener("mouseover", ()=> {
+features.addEventListener("mouseover", ()=> {
   featuresDropdown.classList.add("show");
   featuresArrow.src = "images/icon-arrow-up.svg";
 });
 
-featuresPrev.addEventListener("mouseout", ()=> {
+features.addEventListener("mouseout", ()=> {
   featuresDropdown.classList.remove("show");
   featuresArrow.src = "images/icon-arrow-down.svg";
 });
 
-companyPrev.addEventListener("mouseover", ()=> {
+company.addEventListener("mouseover", ()=> {
   companyDropdown.classList.add("show");
   companyArrow.src = "images/icon-arrow-up.svg";
 });
 
-companyPrev.addEventListener("mouseout", ()=> {
+company.addEventListener("mouseout", ()=> {
   companyDropdown.classList.remove("show");
   companyArrow.src = "images/icon-arrow-down.svg";
 });
